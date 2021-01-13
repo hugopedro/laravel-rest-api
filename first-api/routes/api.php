482d896732a047b40f7597c6868693ba5b789b4b
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2021-01-13 19:26:08 -0300

    Iniciando com API Laravel

--- first-api/routes/api.php
+++ first-api/routes/api.php
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/test', function(Request $request){
+    //dd($request->headers->all());
+	//dd($request->headers->get('Authorization'));
+
+	$response = new \Illuminate\Http\Response(json_encode(['msg' => 'Minha primeira resposta de API']));
+	$response->header('Content-Type', 'application/json');
+
+	return $response;
+});

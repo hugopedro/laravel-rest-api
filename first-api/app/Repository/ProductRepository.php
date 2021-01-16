41f5fd20092d9175d45d7286b108b9be92047207
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2021-01-16 20:25:56 -0300

    Melhoria na lógica parte 2: AbstractRepository

--- first-api/app/Repository/ProductRepository.php
+++ first-api/app/Repository/ProductRepository.php
@@ -2,32 +2,4 @@
 
 namespace App\Repository;
 
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Http\Request;
-
-class ProductRepository {
-
-    private $model;
-
-    public function __construct(Model $model) {
-        $this->model = $model;
-    }
-
-    public function selectConditions($conditions) {
-        $expressions = explode(';', $conditions);
-
-        foreach($expressions as $e) {
-            $exp = explode(':', $e);
-            $this->model = $this->model->where($exp[0], $exp[1], $exp[2]);
-        };
-    }
-
-    public function selectFilter($filters) {
-        $this->model = $this->model->selectRaw($filters);
-    }
-
-    public function getResult() {
-        return $this->model;
-    }
-
-}
+class ProductRepository extends AbstractRepository {}

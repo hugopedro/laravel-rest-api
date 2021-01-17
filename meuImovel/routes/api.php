7e6c4e80ba8e0d699f2b232dc617868c8b692e91
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2021-01-17 17:46:33 -0300

    Iniciando Endpoint

--- meuImovel/routes/api.php
+++ meuImovel/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\RealStateController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('v1')->namespace('Api')->group(function () {
+    Route::prefix('/real-states')->name('real_states.')->group(function () {
+        Route::get('/', [RealStateController::class, 'index']);
+    });
 });

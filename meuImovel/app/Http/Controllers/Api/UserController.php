db13d6d9ee36b9c95c1bc426e4b754057bc4037b
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2021-01-23 16:57:34 -0300

    Fix validator

--- meuImovel/app/Http/Controllers/Api/UserController.php
+++ meuImovel/app/Http/Controllers/Api/UserController.php
@@ -47,7 +47,7 @@ class UserController extends Controller
         Validator::make($data, [
             'phone' => 'required',
             'mobile_phone' => 'required',
-        ]);
+        ])->validate();
 
         try {
             $data['password'] = bcrypt($data['password']);

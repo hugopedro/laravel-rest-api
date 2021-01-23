ac0a6d18019981bf481bd369251832d346452eac
Hugo Pedro <hugogouveia@santos.sp.gov.br>
2021-01-23 15:24:32 -0300

    Atualizando Perfil de Usuário

--- meuImovel/app/Http/Controllers/Api/UserController.php
+++ meuImovel/app/Http/Controllers/Api/UserController.php
@@ -108,9 +108,17 @@ class UserController extends Controller
             unset($data['password']);
         }
 
+        Validator::make($data, [
+            'profile.phone' => 'required',
+            'profile.mobile_phone' => 'required',
+        ]);
+
         try {
+            $profile = $data['profile'];
+            $profile['social_networks'] = serialize($profile['social_networks']);
             $user = $this->user->findOrFail($id);
             $user->update($data); //mass assignment de atualização.
+            $user->profile()->update($profile);
 
             return response()->json([
                 'data' => [
